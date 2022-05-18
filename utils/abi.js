c8cc8eaffe638f26217faf6905ab0ed361730dca
Muhammad Mustapha Sesay <muhammadsesay8@gmail.com>
2022-05-18 01:05:46 +0000

    Add project files

---
+++ utils/abi.js
@@ -0,0 +1,30 @@
+const Abi = [
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+
+export default Abi;
---
+++ utils/index.js
@@ -0,0 +1,57 @@
+const MoodContractAddress = "0x4666851337bc81b0e6e1b771d1c9c4c93fecc4df";
+const MoodContractABI = [
+  {
+    inputs: [],
+    name: "getMood",
+    outputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "_mood",
+        type: "string",
+      },
+    ],
+    name: "setMood",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
+let MoodContract;
+let signer;
+
+const provider = new ethers.providers.Web3Provider(window.ethereum, "ropsten");
+
+provider.send("eth_requestAccounts", []).then(() => {
+  provider.listAccounts().then((accounts) => {
+    signer = provider.getSigner(accounts[0]);
+    MoodContract = new ethers.Contract(
+      MoodContractAddress,
+      MoodContractABI,
+      signer
+    );
+  });
+});
+
+async function getMood() {
+  const getMoodPromise = MoodContract.getMood();
+  const Mood = await getMoodPromise;
+  console.log(Mood);
+}
+
+async function setMood() {
+  const mood = document.getElementById("mood").value;
+  const setMoodPromise = MoodContract.setMood(mood);
+  const sent_mood = await setMoodPromise;
+  console.log(sent_mood);
+}
